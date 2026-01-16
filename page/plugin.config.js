601e5fbe75a3736f6a73fe0614813a962c125823
Christoph <christophhummer@gmail.com>
2026-01-16 09:36:03 +0100

    Update plugin.config.js

--- page/plugin.config.js
+++ page/plugin.config.js
@@ -3,7 +3,7 @@ export default {
   displayName: 'SR-IOV i915 Driver',
   description: 'SR-IOV i915 Driver for MOS',
   version: '0.0.0',
-  icon: 'mdi-align-vertical-distribute',
+  icon: 'https://raw.githubusercontent.com/ich777/docker-templates/master/ich777/images/intel.png',
   author: 'ich777',
-  homepage: 'https://raw.githubusercontent.com/ich777/docker-templates/master/ich777/images/intel.png',
+  homepage: 'https://github.com/strongtz/i915-sriov-dkms',
 };
